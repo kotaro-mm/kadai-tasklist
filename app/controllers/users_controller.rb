0ba3fda74e7c2d21945ab90a1d06ffa57749096b
kotaro.m <benkyou1212@gmail.com>
2021-02-04 08:53:51 +0000

    index_show_delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,4 @@
 class UsersController < ApplicationController
-  # def index
-  #   @users = User.order(id: :desc).page(params[:page]).per(25)
-  # end
-
-  # def show
-  #   @user = User.find(params[:id])
-  #   @tasks = @user.tasks.all
-  # end
-
   def new
     @user = User.new
   end
